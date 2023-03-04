cee7fbf9242b1dfec547ff42742432090f18ccd4
eplq <91148468+eplq@users.noreply.github.com>
2023-03-04 13:47:50 +0100

    add emails and phones to person

---
+++ packages/server/prisma/migrations/20230304123140_create_user_emails_and_phones_tables/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - Added the required column `userPersonId` to the `Person` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Person` ADD COLUMN `userPersonId` INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE `PersonPhone` (
+    `personId` INTEGER NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `PersonPhone_personId_phone_key`(`personId`, `phone`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PersonEmail` (
+    `personId` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `PersonEmail_personId_email_key`(`personId`, `email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `PersonPhone` ADD CONSTRAINT `PersonPhone_personId_fkey` FOREIGN KEY (`personId`) REFERENCES `Person`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PersonEmail` ADD CONSTRAINT `PersonEmail_personId_fkey` FOREIGN KEY (`personId`) REFERENCES `Person`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
