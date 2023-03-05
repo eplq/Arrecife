e5cc3ad97acd54ca6a08b4673527f75037c9af7b
eplq <91148468+eplq@users.noreply.github.com>
2023-03-05 18:42:56 +0100

    create Building and ProductStock models

---
+++ packages/server/prisma/migrations/20230305174143_create_building_and_inventory_tables/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[code]` on the table `Product` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateTable
+CREATE TABLE `Building` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(70) NOT NULL,
+    `address` VARCHAR(70) NOT NULL,
+    `phone` VARCHAR(18) NOT NULL,
+    `type` ENUM('WAREHOUSE', 'STORE') NOT NULL DEFAULT 'STORE',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ProductStock` (
+    `buildingId` INTEGER NOT NULL,
+    `productId` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`buildingId`, `productId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Product_code_key` ON `Product`(`code`);
+
+-- AddForeignKey
+ALTER TABLE `ProductStock` ADD CONSTRAINT `ProductStock_buildingId_fkey` FOREIGN KEY (`buildingId`) REFERENCES `Building`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ProductStock` ADD CONSTRAINT `ProductStock_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`code`) ON DELETE RESTRICT ON UPDATE CASCADE;
