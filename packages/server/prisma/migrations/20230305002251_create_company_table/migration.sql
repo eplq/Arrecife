114890c6cc493006acea85ad37dbdf31ddb1d83e
eplq <91148468+eplq@users.noreply.github.com>
2023-03-05 01:23:39 +0100

    create Company model

---
+++ packages/server/prisma/migrations/20230305002251_create_company_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `UserCompany` (
+    `userId` INTEGER NOT NULL,
+    `companyId` INTEGER NOT NULL,
+    `managesIt` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`userId`, `companyId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Company` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(80) NOT NULL,
+    `NIF` VARCHAR(10) NOT NULL,
+    `address` VARCHAR(70) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `UserCompany` ADD CONSTRAINT `UserCompany_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`personId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `UserCompany` ADD CONSTRAINT `UserCompany_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
