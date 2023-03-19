2125831d494ea84afb8c25b568ba4f912ff71819
eplq <91148468+eplq@users.noreply.github.com>
2023-03-19 13:02:30 +0100

    install cookie-parser package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import cookieParser from 'cookie-parser';
+import express from 'express';
 
-const app = express()
+const app = express();
+app.use(cookieParser());
 
-app.get("/", (_req, res) => {
-    res.status(200).send("testing")
-})
+app.get('/', (_req, res) => {
+    res.status(200).send('testing');
+});
 
 app.listen(3000, () => {
-    console.log("listening")
-})
+    console.log('listening');
+});
