895f70207bba2074e25faf01aeb538a7675f2de2
eplq <91148468+eplq@users.noreply.github.com>
2023-04-03 19:15:18 +0200

    create contacts procedure

---
+++ packages/server/src/routers/contacts.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+import prisma from '../prisma';
+import authedProcedure from '../procedures/authedProcedure';
+import { router } from '../trpc';
+
+const contactsRouter = router({
+    contacts: authedProcedure
+        .input(z.number().int().positive())
+        .query(async ({ ctx, input }) => {
+            const company = await prisma.company.findFirst({
+                where: {
+                    id: input,
+                    users: {
+                        some: {
+                            personId: ctx.session.id
+                        }
+                    },
+                    owner: null
+                }
+            });
+
+            if (!company) return null;
+
+            const contacts = await prisma.contact.findMany({
+                where: {
+                    companyId: company.id
+                }
+            });
+
+            if (!contacts) return null;
+
+            return contacts;
+        })
+});
+
+export default contactsRouter;
