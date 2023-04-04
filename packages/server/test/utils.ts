73c3bc80b1cc09209bce7b5e6536106343c23a93
eplq <91148468+eplq@users.noreply.github.com>
2023-04-04 17:11:18 +0200

    add emails and phones to db reset for tests

--- packages/server/test/utils.ts
+++ packages/server/test/utils.ts
@@ -5,6 +5,8 @@ export default async function resetDB(prisma: PrismaClient) {
     await prisma.company.deleteMany();
     await prisma.userSession.deleteMany();
     await prisma.user.deleteMany();
+    await prisma.personEmail.deleteMany();
+    await prisma.personPhone.deleteMany();
     await prisma.person.deleteMany();
     await prisma.$queryRaw`ALTER TABLE Person AUTO_INCREMENT = 0`;
     await prisma.$queryRaw`ALTER TABLE Company AUTO_INCREMENT = 0`;
