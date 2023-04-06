140032cbac6a88bb5ed9aef5a557a185668f0bcf
eplq <91148468+eplq@users.noreply.github.com>
2023-04-06 23:26:45 +0200

    correct Tax schema

--- packages/server/src/schemas/tax.ts
+++ packages/server/src/schemas/tax.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const TaxSchema = z.object({
     name: z.string().min(1),
-    rate: z.number().min(0).max(1).default(0)
+    rate: z.number().int().min(0).max(100).default(0)
 });
 
 export type TaxType = z.infer<typeof TaxSchema>;
