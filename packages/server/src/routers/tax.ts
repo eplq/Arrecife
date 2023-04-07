1ec16f798b3afa0603d983f5d23edd94fc37ec9e
eplq <91148468+eplq@users.noreply.github.com>
2023-04-07 13:21:40 +0200

    write addTax procedure

--- packages/server/src/routers/tax.ts
+++ packages/server/src/routers/tax.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 import prisma from '../prisma';
 import authedProcedure from '../procedures/authedProcedure';
+import { TaxSchema } from '../schemas/tax';
 import { router } from '../trpc';
 
 const taxRouter = router({
@@ -28,6 +29,47 @@ const taxRouter = router({
             });
 
             return taxes;
+        }),
+
+    addTax: authedProcedure
+        .input(
+            TaxSchema.extend({
+                company: z.number().int().positive()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const company = await prisma.company.findFirst({
+                where: {
+                    id: input.company,
+                    users: {
+                        some: {
+                            personId: ctx.session.id
+                        }
+                    }
+                }
+            });
+
+            if (!company) return false;
+
+            const tax = await prisma.tax.findFirst({
+                where: {
+                    companyId: company.id,
+                    name: input.name,
+                    rate: input.rate
+                }
+            });
+
+            if (tax) return false;
+
+            const newTax = await prisma.tax.create({
+                data: {
+                    name: input.name,
+                    rate: input.rate,
+                    companyId: company.id
+                }
+            });
+
+            return !!newTax;
         })
 });
 
