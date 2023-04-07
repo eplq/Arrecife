f90b7754ba4295dd62fd020f8538ce082a7dff4d
eplq <91148468+eplq@users.noreply.github.com>
2023-04-07 13:35:13 +0200

    write modifyTax procedure

--- packages/server/src/routers/tax.ts
+++ packages/server/src/routers/tax.ts
@@ -109,6 +109,48 @@ const taxRouter = router({
             });
 
             return true;
+        }),
+    modifyTax: authedProcedure
+        .input(
+            TaxSchema.extend({
+                tax: z.number().int().positive(),
+                company: z.number().int().positive()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const company = await prisma.company.findFirst({
+                where: {
+                    id: input.company,
+                    users: {
+                        some: {
+                            personId: ctx.session.id
+                        }
+                    }
+                }
+            });
+
+            if (!company) return false;
+
+            const tax = await prisma.tax.findFirst({
+                where: {
+                    id: input.tax,
+                    companyId: company.id
+                }
+            });
+
+            if (!tax) return false;
+
+            await prisma.tax.update({
+                where: {
+                    id: tax.id
+                },
+                data: {
+                    name: input.name,
+                    rate: input.rate
+                }
+            });
+
+            return true;
         })
 });
 
