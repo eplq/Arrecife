2a2b8a334e0fe03ff2ceadefebf7f310f9705054
eplq <91148468+eplq@users.noreply.github.com>
2023-04-08 12:49:45 +0200

    write paymentPlan procedure

--- packages/server/src/routers/paymentPlan.ts
+++ packages/server/src/routers/paymentPlan.ts
@@ -33,6 +33,39 @@ const paymentPlansRouter = router({
             if (!paymentPlans.length) return null;
 
             return paymentPlans;
+        }),
+
+    paymentPlan: authedProcedure
+        .input(
+            z.object({
+                company: z.number().int().positive(),
+                paymentPlan: z.number().int().positive()
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const company = await prisma.company.findFirst({
+                where: {
+                    id: input.company,
+                    users: {
+                        some: {
+                            personId: ctx.session.id
+                        }
+                    }
+                }
+            });
+
+            if (!company) return null;
+
+            const paymentPlan = await prisma.paymentPlan.findFirst({
+                where: {
+                    id: company.id
+                },
+                include: {
+                    payments: {}
+                }
+            });
+
+            return paymentPlan;
         })
 });
 
