56d4c21cb40e2c7d1cc709d8ac628a285a233d1b
eplq <91148468+eplq@users.noreply.github.com>
2023-04-11 20:00:42 +0200

    write createPaymentPlan procedure

--- packages/server/src/routers/paymentPlan.ts
+++ packages/server/src/routers/paymentPlan.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 import prisma from '../prisma';
 import authedProcedure from '../procedures/authedProcedure';
+import { PaymentPlanSchema } from '../schemas/paymentPlan';
 import { router } from '../trpc';
 
 const paymentPlansRouter = router({
@@ -62,6 +63,50 @@ const paymentPlansRouter = router({
                 },
                 include: {
                     payments: {}
+                }
+            });
+
+            return paymentPlan;
+        }),
+
+    createPaymentPlan: authedProcedure
+        .input(
+            PaymentPlanSchema.extend({
+                company: z.number().int().positive()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const company = await prisma.company.findFirst({
+                where: {
+                    id: input.company,
+                    users: {
+                        some: {
+                            personId: ctx.session.id
+                        }
+                    }
+                }
+            });
+
+            if (!company) return null;
+
+            const paymentPlan = await prisma.paymentPlan.findFirst({
+                where: {
+                    id: company.id,
+                    name: input.name
+                }
+            });
+
+            if (paymentPlan) return null;
+
+            await prisma.paymentPlan.create({
+                data: {
+                    name: input.name,
+                    companyId: company.id,
+                    payments: {
+                        createMany: {
+                            data: input.payments
+                        }
+                    }
                 }
             });
 
--- packages/server/src/schemas/paymentPlan.ts
+++ packages/server/src/schemas/paymentPlan.ts
@@ -1,15 +1,16 @@
 import { z } from 'zod';
 
-const PaymentPlanPayment = z.object({
+const PaymentPlanPaymentSchema = z.object({
     days: z.number().int().nonnegative(),
     percentage: z.number().int().min(0).max(100)
 });
 
 const PaymentPlanSchema = z.object({
     name: z.string().min(1),
-    payments: z.array(PaymentPlanPayment)
+    payments: z.array(PaymentPlanPaymentSchema)
 });
 
-export { PaymentPlanSchema };
+export { PaymentPlanSchema, PaymentPlanPaymentSchema };
 
 export type PaymentPlanType = z.infer<typeof PaymentPlanSchema>;
+export type PaymentPlanPaymentType = z.infer<typeof PaymentPlanPaymentSchema>;
