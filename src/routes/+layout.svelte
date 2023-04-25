8ef2a36b631d9f47dddc87612e3c657711885551
eplq <91148468+eplq@users.noreply.github.com>
2023-04-25 20:19:23 +0200

    use bootstrap in layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import bootstrapUrl from 'bootstrap/dist/js/bootstrap.bundle?url';
+
+	console.log(bootstrapUrl);
+</script>
+
+<svelte:head>
+	<script src={bootstrapUrl}></script>
+</svelte:head>
+
+<slot />
