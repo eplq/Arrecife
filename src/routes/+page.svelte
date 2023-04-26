88ade8d36475819c0f6730e97ea2c9a16f28b36e
eplq <91148468+eplq@users.noreply.github.com>
2023-04-26 20:23:39 +0200

    make simple login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
 	<h1>Arrecife</h1>
 	<div>
-		<a class="btn btn-outline-primary" href="/login">Login</a>
+		<a class="btn btn-outline-primary" href="/auth/login">Login</a>
 		<a class="btn btn-outline-primary" href="/register">Register</a>
 	</div>
 </div>
