344d36377a2624337e6744dce7b02958864d34b6
eplq <91148468+eplq@users.noreply.github.com>
2023-04-30 19:34:28 +0200

    create protected layout

---
+++ src/routes/(protected)/+layout.server.ts
@@ -0,0 +1,13 @@
+import { redirect } from '@sveltejs/kit';
+
+import type { LayoutServerLoad } from './$types';
+
+export const load = (async ({ locals: { user } }) => {
+	if (!user) {
+		throw redirect(302, '/auth/login');
+	}
+
+	return {
+		...user
+	};
+}) satisfies LayoutServerLoad;
