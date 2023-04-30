69eb420fe5f5d5e39c719eb7c3bdf96ae6210ac4
eplq <91148468+eplq@users.noreply.github.com>
2023-04-30 23:16:58 +0200

    add bootstrap icons css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import 'bootstrap-icons/font/bootstrap-icons.min.css';
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import bootstrapUrl from 'bootstrap/dist/js/bootstrap.bundle?url';
 </script>
