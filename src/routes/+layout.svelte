0f3fdb261446b6c6c41ba7022db347ef182a765f
eplq <91148468+eplq@users.noreply.github.com>
2023-04-30 19:46:06 +0200

    uninstall bootstrap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-	import 'bootstrap/dist/css/bootstrap.min.css';
-	import bootstrapUrl from 'bootstrap/dist/js/bootstrap.bundle?url';
-</script>
-
-<svelte:head>
-	<script src={bootstrapUrl}></script>
-</svelte:head>
-
 <slot />
