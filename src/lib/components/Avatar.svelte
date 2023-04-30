7b5d66d13c8ec0983acb94e8d7324d9f3af04078
eplq <91148468+eplq@users.noreply.github.com>
2023-04-30 22:16:57 +0200

    write avatar component

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let name: string;
+
+	let firstCharacter = 'A';
+	$: firstCharacter = name.length ? name.charAt(0) : 'A';
+</script>
+
+<div class="fw-bolder border rounded squared d-flex justify-content-center align-items-center">
+	{firstCharacter}
+</div>
+
+<style>
+	.squared {
+		width: 32px;
+		height: 32px;
+	}
+</style>
