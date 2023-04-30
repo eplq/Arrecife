8fe1080f3d22afe46f63a6c0608b939893015d09
eplq <91148468+eplq@users.noreply.github.com>
2023-04-30 19:56:13 +0200

    Revert "uninstall bootstrap"
    This reverts commit 0f3fdb261446b6c6c41ba7022db347ef182a765f.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import bootstrapUrl from 'bootstrap/dist/js/bootstrap.bundle?url';
+</script>
+
+<svelte:head>
+	<script src={bootstrapUrl}></script>
+</svelte:head>
+
 <slot />
