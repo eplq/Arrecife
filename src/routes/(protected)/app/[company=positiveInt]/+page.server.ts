0e122dbfb22d6b36d75d2e520d378b0c6e0d19a9
eplq <91148468+eplq@users.noreply.github.com>
2023-05-01 23:30:19 +0200

    ensure currentCompany is defined when in /app/n

--- src/routes/(protected)/app/[company=positiveInt]/+page.server.ts
+++ src/routes/(protected)/app/[company=positiveInt]/+page.server.ts
@@ -1,14 +1,38 @@
+import prisma from '$lib/server/prisma';
 import { error } from '@sveltejs/kit';
 
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, parent }) => {
-	const { companies } = await parent();
+	const { companies, user } = await parent();
 
 	const company = companies.find((item) => item.id.toString() === params.company);
 
+	let currentCompany = null;
 	if (company) {
-		return {};
+		currentCompany = await prisma.company.findFirst({
+			where: {
+				users: {
+					some: {
+						id: user.id
+					}
+				},
+				id: Number(company.id),
+				owner: null
+			},
+			select: {
+				id: true,
+				name: true,
+				NIF: true,
+				address: true
+			}
+		});
+	}
+
+	if (currentCompany) {
+		return {
+			currentCompany
+		};
 	}
 
 	throw error(404, 'not found');
