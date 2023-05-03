a1f7442e4ce51d58166e901b5520aff2e198c7cc
eplq <91148468+eplq@users.noreply.github.com>
2023-05-03 19:53:02 +0200

    INCOMPLETE: create companies view

---
+++ src/routes/(protected)/app/[company=positiveInt]/companies/+page.server.ts
@@ -0,0 +1,17 @@
+import prisma from '$lib/server/prisma';
+
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ parent }) => {
+	const { currentCompany } = await parent();
+
+	const companies = await prisma.company.findMany({
+		where: {
+			ownerId: currentCompany?.id
+		}
+	});
+
+	return {
+		companies
+	};
+}) satisfies PageServerLoad;
---
+++ src/routes/(protected)/app/[company=positiveInt]/companies/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const currentPath = $page.url.pathname;
+</script>
+
+{JSON.stringify(data)}
+
+<h1>Empresas</h1>
+
+<div class="mt-3 d-flex">
+	<a class="ms-auto btn btn-primary" href={`${currentPath}/add`}>Añadir empresa</a>
+</div>
+
+<div class="mt-3 flex-grow-1 overflow-auto">
+	<table class="table table-hover table-strip">
+		<thead>
+			<tr>
+				<th scope="col">NIF</th>
+				<th scope="col">Nombre</th>
+				<th scope="col">Dirección</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.companies as company}
+				<tr>
+					<th scope="row">{company.NIF}</th>
+					<td>{company.name}</td>
+					<td>{company.address}</td>
+				</tr>
+			{/each}
+			{#each data.companies as company}
+				<tr>
+					<th scope="row">{company.NIF}</th>
+					<td>{company.name}</td>
+					<td>{company.address}</td>
+				</tr>
+			{/each}
+			{#each data.companies as company}
+				<tr>
+					<th scope="row">{company.NIF}</th>
+					<td>{company.name}</td>
+					<td>{company.address}</td>
+				</tr>
+			{/each}
+			{#each data.companies as company}
+				<tr>
+					<th scope="row">{company.NIF}</th>
+					<td>{company.name}</td>
+					<td>{company.address}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
