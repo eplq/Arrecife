2a2adae40a3a14548a53cc417a0885357409c3a6
eplq <91148468+eplq@users.noreply.github.com>
2023-06-07 20:10:58 +0200

    create main page company selector

--- src/routes/(protected)/app/+page.svelte
+++ src/routes/(protected)/app/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Pues eso</h1>
+<script lang="ts">
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
+</script>
+
+<h1>Hola, {data.user.name}</h1>
+
+<p>Seleccione una empresa para empezar a trabajar</p>
+
+<div class="flex-grow-1 overflow-auto">
+	<ul>
+		{#if data.companies.length === 0}
+			<li>No hay empresas disponibles</li>
+		{/if}
+		{#each data.companies as company}
+			<li class="mt-2">
+				<a class="btn btn-outline-secondary" href={`/app/${company.id}/`}>{company.name}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
