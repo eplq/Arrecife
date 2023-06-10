be39307e8716c59a6d89fbf84c98684e23a7353f
eplq <91148468+eplq@users.noreply.github.com>
2023-06-10 20:27:55 +0200

    improve main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,36 @@
-<div class="vh-100 d-flex flex-column justify-content-center align-items-center">
-	<h1>Arrecife</h1>
-	<div>
-		<a class="btn btn-outline-secondary" href="/auth/login">Iniciar sesión</a>
-		<a class="btn btn-outline-secondary" href="/auth/register">Registrarse</a>
+<div class="fv-container vh-100 d-flex flex-column justify-content-center align-items-center">
+	<div
+		class="content-container d-flex flex-column justify-content-center align-items-center bg-light rounded-1"
+	>
+		<h1>Arrecife</h1>
+		<div>
+			<a class="btn btn-secondary" href="/auth/login">Iniciar sesión</a>
+			<a class="btn btn-secondary" href="/auth/register">Registrarse</a>
+		</div>
 	</div>
 </div>
+<a class="photo-link" href="https://unsplash.com/es/fotos/2mH5fEFYc5s"
+	>Foto de Unsplash por Verónica Reverse</a
+>
+
+<style>
+	.fv-container {
+		background-image: url('/bg.jpg');
+		background-size: cover;
+	}
+
+	.content-container {
+		--bs-bg-opacity: 0.85;
+		width: 30%;
+		height: 20%;
+	}
+
+	.photo-link {
+		font-size: 0.75rem;
+		color: #dbdbdb;
+
+		position: absolute;
+		bottom: 1rem;
+		right: 1rem;
+	}
+</style>
