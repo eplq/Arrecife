14b220031c10594a6937a6f668eb3ca7dded5ca2
eplq <91148468+eplq@users.noreply.github.com>
2023-06-10 20:04:20 +0200

    write modify article page

--- src/routes/(protected)/app/[company=positiveInt]/articles/modify/[article]/+page.server.ts
+++ src/routes/(protected)/app/[company=positiveInt]/articles/modify/[article]/+page.server.ts
@@ -1,7 +1,7 @@
 import prisma from '$lib/server/prisma';
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ parent, params }) => {
 	const { currentCompany } = await parent();
@@ -17,6 +17,8 @@ export const load: PageServerLoad = async ({ parent, params }) => {
 		}
 	});
 
+	if (!brands.length) return fail(400);
+
 	const article = await prisma.product.findFirst({
 		where: {
 			code: articleParam,
@@ -31,4 +33,105 @@ export const load: PageServerLoad = async ({ parent, params }) => {
 	if (!article) return fail(400);
 
 	return { brands, article };
+};
+
+export const actions: Actions = {
+	default: async ({ request, params, locals: { user } }) => {
+		const formData = await request.formData();
+		const name = formData.get('name');
+		const code = formData.get('code');
+		const internalCode = formData.get('internal_code');
+		const cost = formData.get('cost');
+		const price = formData.get('price');
+		const brand = formData.get('brand');
+
+		if (!name || typeof name !== 'string') {
+			return fail(400, { code, internalCode, cost, price, brand, nameMissing: true });
+		}
+
+		if (!code || typeof code !== 'string') {
+			return fail(400, { name, internalCode, cost, price, brand, codeMissing: true });
+		}
+
+		if (!internalCode || typeof internalCode !== 'string') {
+			return fail(400, { name, code, cost, price, brand, internalCodeMissing: true });
+		}
+
+		if (!price || typeof price !== 'string') {
+			return fail(400, { name, code, internalCode, cost, brand, priceMissing: true });
+		}
+
+		if (cost && typeof cost !== 'string') {
+			return fail(400, { name, code, internalCode, price, brand });
+		}
+
+		if (!brand || typeof brand !== 'string') {
+			return fail(400, { name, code, internalCode, cost, price, brandMissing: true });
+		}
+
+		const priceInt = parseFloat(price) * 100;
+		const costInt = cost ? parseFloat(cost) * 100 : null;
+
+		const brandId = parseInt(brand);
+
+		const brandEntity = await prisma.brand.findFirst({
+			where: {
+				id: brandId
+			}
+		});
+
+		if (!brandEntity) {
+			return fail(400, { name, code, internalCode, cost, price, brand, brandMissing: true });
+		}
+
+		const { company, article } = params;
+
+		const articleEntity = await prisma.product.findFirst({
+			where: {
+				code: article,
+				brand: {
+					provider: {
+						company: {
+							owner: {
+								users: {
+									some: {
+										id: user?.id
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		});
+
+		if (!articleEntity) {
+			return fail(400, {
+				name,
+				code,
+				internalCode,
+				cost,
+				price,
+				brand,
+				article,
+				articleMissing: true
+			});
+		}
+
+		await prisma.product.update({
+			where: {
+				code: articleEntity.code
+			},
+			data: {
+				name,
+				code,
+				internal_code: internalCode,
+				cost: Math.round(costInt!),
+				price: Math.round(priceInt!),
+				brand: { connect: { id: brandEntity.id } }
+			}
+		});
+
+		throw redirect(301, `/app/${company}/articles`);
+	}
 };
---
+++ src/routes/(protected)/app/[company=positiveInt]/articles/modify/[article]/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+</script>
+
+<h1>Modificar artículo</h1>
+
+<form method="post" use:enhance>
+	<div class="mb-3">
+		<label for="name" class="form-label">Nombre</label>
+		<input
+			type="text"
+			class="form-control"
+			name="name"
+			id="name"
+			placeholder="Nombre"
+			value={data.article?.name}
+		/>
+	</div>
+
+	<div class="mb-3">
+		<label for="code" class="form-label">Código de barras</label>
+		<input
+			type="text"
+			name="code"
+			id="code"
+			class="form-control"
+			placeholder="Código de barras"
+			value={data.article?.code}
+		/>
+	</div>
+
+	<div class="mb-3">
+		<label for="internal_code" class="form-label">Código interno</label>
+		<input
+			type="text"
+			name="internal_code"
+			id="internal_code"
+			class="form-control"
+			placeholder="Código interno"
+			value={data.article?.internal_code}
+		/>
+	</div>
+
+	<div class="mb-3">
+		<label for="cost" class="form-label">Costo</label>
+		<input
+			type="number"
+			class="form-control"
+			id="cost"
+			name="cost"
+			value={data.article?.cost ? data.article?.cost / 100 : 0}
+			placeholder="Costo"
+			min="0"
+			step="0.01"
+		/>
+	</div>
+
+	<div class="mb-3">
+		<label for="price" class="form-label">Precio (sin IVA)</label>
+		<input
+			type="number"
+			class="form-control"
+			id="price"
+			name="price"
+			placeholder="Precio (sin IVA)"
+			value={data.article?.price ? data.article?.price / 100 : 0}
+			min="0"
+			step="0.01"
+			required
+		/>
+	</div>
+
+	<div class="mb-3">
+		<label for="brand" class="form-label">Marca</label>
+		<select
+			class="form-select"
+			name="brand"
+			id="brand"
+			placeholder="Marca"
+			value={data.article?.brandId}
+		>
+			{#if data.brands}
+				{#each data.brands as brand}
+					<option value={brand.id}>{brand.name}</option>
+				{/each}
+			{/if}
+		</select>
+	</div>
+
+	<input type="submit" value="Modificar" class="btn btn-primary" />
+</form>
